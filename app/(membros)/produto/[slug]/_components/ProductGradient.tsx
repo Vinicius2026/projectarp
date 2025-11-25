c24d8822a74a8eaf3bdaf054f58d9cee91d455c8
Vinicius2026 <jasvinigaming@gmail.com>
2025-11-25 17:29:25 -0300

    Background 4

--- app/(membros)/produto/[slug]/_components/ProductGradient.tsx
+++ app/(membros)/produto/[slug]/_components/ProductGradient.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useTheme } from '../../contexts/ThemeContext'
+import { useTheme } from '../../../contexts/ThemeContext'
 
 interface ProductGradientProps {
   color: string
